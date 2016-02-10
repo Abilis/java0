b0835e5b64def2254bd743995114ee14fd2f4c77
Abilis <abilis@mail.ru>
2016-02-11 01:26:38 +0400

    Тренировка с переменными

--- Lesson 1/src/MainClass.java
+++ Lesson 1/src/MainClass.java
@@ -1,25 +1,29 @@
 /**
- * Created by Admin on 10.02.2016.
+ * Created by Abilis on 10.02.2016.
  */
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
+        byte a = 13; //8 Бит
+        short b = 25; //16 бит
+        int c = 17; //32 бит
+        long d = 255; //64 бита
 
-        int a = 10;
-        int b = 20;
+        float e = 15.24f; //32 бит
+        double f = 2.6; //64 бита
 
-        sum(a, b);
+        boolean g = true; //1 Бит
+
+        System.out.println("Byte: " + a);
+        System.out.println("Short: " + b);
+        System.out.println("Int: " + c);
+        System.out.println("Long: " + d);
+        System.out.println("Float: " + e);
+        System.out.println("Double: " + f);
+        System.out.println("Boolean: " + g);
 
     }
-
-
-    public static void sum(int a, int b) {
-        System.out.println(a + b);
-    }
-
-
-
 
 
 }
