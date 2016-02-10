30221948a0bdbb1393a17b681777ed0cb8acd1c0
Abilis <abilis@mail.ru>
2016-02-11 00:04:43 +0400

    Lesson 1

---
+++ Lesson 1/src/MainClass.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Admin on 10.02.2016.
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+
+        sum(a, b);
+
+    }
+
+
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+
+
+
+
+
+}
