0f732023f8bd8bad78ea61347fc94ececc4321dd
Abilis <abilis@mail.ru>
2016-02-11 23:51:42 +0400

    Операции с массивами

--- Lesson 1/src/MainClass.java
+++ Lesson 1/src/MainClass.java
@@ -3,25 +3,76 @@
  */
 public class MainClass {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        byte a = 13; //8 Бит
-        short b = 25; //16 бит
-        int c = 17; //32 бит
-        long d = 255; //64 бита
+        int[] testarr = new int[79];
 
-        float e = 15.24f; //32 бит
-        double f = 2.6; //64 бита
+        for (int i = 0; i < testarr.length; i++) {
+            testarr[i] = (int)(Math.random() * 101); // гереним случайное целое число от 0 до 100
+        }
 
-        boolean g = true; //1 Бит
+        int max = 100; //максимальное и минимальное значения в массиве
+        int min = 0;
 
-        System.out.println("Byte: " + a);
-        System.out.println("Short: " + b);
-        System.out.println("Int: " + c);
-        System.out.println("Long: " + d);
-        System.out.println("Float: " + e);
-        System.out.println("Double: " + f);
-        System.out.println("Boolean: " + g);
+        printArr(testarr); //рисуем массив на экране
+
+        int minFromArr = getMinFromArr(testarr, max); //Получаем минимальное значение в массиве
+        int maxFromArr = getMaxFromArr(testarr, min); //Получаем максимальное значение в массиве
+        int averFromArr = getAverageFromArr(testarr); //Получаем среднее значение в массиве
+
+        System.out.println("Минимальное значение в массиве: " + minFromArr);
+        System.out.println("Максимальное значение в массиве: " + maxFromArr);
+        System.out.println("Среднее значение в массиве " + averFromArr);
+
+        
+
+    }
+
+    public static void printArr(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (((i % 50) == 0) && (i != 0)) {
+                System.out.println();
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+    }
+    
+    public static int getMinFromArr(int arr[], int max) {
+
+        for (int i: arr) {
+            if (i < max) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static int getMaxFromArr(int arr[], int min) {
+
+        for (int i: arr) {
+            if (i > min) {
+                min = i;
+            }
+        }
+
+        return min;
+
+    }
+
+    public static int getAverageFromArr(int arr[]) {
+
+        int sumOfArr = 0;
+        for (int i: arr) {
+             sumOfArr += i;
+        }
+
+        int averOfArr = sumOfArr / arr.length;
+
+        return averOfArr;
 
     }
 
