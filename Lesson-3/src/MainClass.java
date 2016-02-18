1e52f8bd6d7da378f1ae5f11fec339a097deb76c
Abilis <abilis@mail.ru>
2016-02-18 23:53:22 +0400

    Ничего не изменилось

--- Lesson-3/src/MainClass.java
+++ Lesson-3/src/MainClass.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+
 /**
  * Created by Abilis on 18.02.2016.
  */
 public class MainClass {
 
     public static void main(String[] args) {
+
+        ArrayList<String> str1 = new ArrayList<>();
+
+        str1.add("hello");
+        str1.add("world");
+
+        System.out.println(str1);
+
+        str1.add(0, "bye");
+        System.out.println(str1);
+
+        str1.set(0, "test");
+        System.out.println(str1);
 
     }
 
